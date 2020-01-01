b37cdae22044461bed9e866348845dfa98ad58ad
Kien Nguyen <kc97ble@gmail.com>
2020-01-01 12:19:50 +0700

    Ninth commit

--- src/icrew/settings.py
+++ src/icrew/settings.py
@@ -88,10 +88,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
+    # {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
+    # {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
+    # {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
+    # {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
 
 
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Singapore'
 
 USE_I18N = True
 
